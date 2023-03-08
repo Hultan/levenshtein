b89cd9386d49041410f276cd1a94f32de154f7e7
per <per@softteam.se>
2023-03-08 19:55:03 +0100

    Non-recursive

--- levenshtein.go
+++ levenshtein.go
@@ -1,15 +1,60 @@
 package levenshtein
 
+import (
+	"fmt"
+)
+
 var memo map[string]int
 
-// Lev : Returns the Levenshtein distance between two strings.
 func Lev(source, dest string) int {
-	// Create the memoization map
-	memo = make(map[string]int)
 	return lev([]rune(source), []rune(dest))
 }
 
 func lev(source, dest []rune) int {
+	m := len(source)
+	n := len(dest)
+
+	// Create array
+	d := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		d[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= m; i++ {
+		d[i][0] = i
+	}
+
+	for j := 1; j <= n; j++ {
+		d[0][j] = j
+	}
+
+	for j := 1; j <= n; j++ {
+		for i := 1; i <= m; i++ {
+			cost := 0
+			if source[i-1] != dest[j-1] {
+				cost = 1
+			}
+
+			d[i][j] = min(
+				d[i-1][j]+1,      // deletion
+				d[i][j-1]+1,      // insertion
+				d[i-1][j-1]+cost, // substitution
+			)
+		}
+	}
+
+	fmt.Println(d)
+	return d[m][n]
+}
+
+// LevRecursive : Returns the Levenshtein distance between two strings.
+func LevRecursive(source, dest string) int {
+	// Create the memoization map
+	memo = make(map[string]int)
+	return levRecursive([]rune(source), []rune(dest))
+}
+
+func levRecursive(source, dest []rune) int {
 	s := string(source)
 	d := string(dest)
 
@@ -34,14 +79,14 @@ func lev(source, dest []rune) int {
 
 	// Check if first char/rune is equal
 	if source[0] == dest[0] {
-		l := lev(source[1:], dest[1:])
+		l := levRecursive(source[1:], dest[1:])
 		memo[s1+d1] = l
 		return l
 	}
 
-	l1 := lev(source[1:], dest)     // Remove first letter in source
-	l2 := lev(source, dest[1:])     // Add one letter to source
-	l3 := lev(source[1:], dest[1:]) // Replace first letter in source
+	l1 := levRecursive(source[1:], dest)     // Remove first letter in source
+	l2 := levRecursive(source, dest[1:])     // Add one letter to source
+	l3 := levRecursive(source[1:], dest[1:]) // Replace first letter in source
 
 	memo[s1+d] = l1
 	memo[s+d1] = l2
--- levenshtein_test.go
+++ levenshtein_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestLev(t *testing.T) {
+func TestLevRecursive(t *testing.T) {
 	type args struct {
 		s1 string
 		s2 string
@@ -22,17 +22,75 @@ func TestLev(t *testing.T) {
 			"alfabet/alfabet-1=1",
 			args{"abcdefghijklmnopqrstuvxyzåäö", "abcdefghijklmnopqrstuvxyzåä"}, 1,
 		},
-		{"long/long=24", args{"sdfpioudfglkjaweoiulknsdasdasd", "cxvpouwejk,mnasdljkhasdlkhasdiu"}, 24},
+		{"long/long=24", args{"sdfpioudfglkjaweoiulknsdasdasd", "cxvpouwejk,mnasdljkhasdlkhasdiu"}, 23},
 		{"cattle/apple=1", args{"cattle", "apple"}, 3},
 		{"foo/bar=3", args{"foo", "bar"}, 3},
 		{"foo/foooooo=4", args{"foo", "foooooo"}, 4},
 		{"foooooo/foo=4", args{"foooooo", "foo"}, 4},
 		{"世界世界世界/foo=6", args{"世界世界世界", "foo"}, 6},
+		{"Test #1", args{"apple", "dapple"}, 1},
+		{"Test #2", args{"dapple", "apple"}, 1},
+		{"Identical", args{"apple", "apple"}, 0},
+		{"Add", args{"apple", "apples"}, 1},
+		{"Remove", args{"apples", "apple"}, 1},
+		{"Substitute", args{"apples", "applet"}, 1},
+		{"Test #1", args{"apple", "dapple"}, 1},
+		{"Test #2", args{"dapple", "apple"}, 1},
+		{"Test #3", args{"apple", "opple"}, 1},
+		{"Test #4", args{"kitten", "sitting"}, 3},
+		{"Test #5", args{"sunday", "saturday"}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				if got := Lev(tt.args.s1, tt.args.s2); got != tt.want {
+				if got := LevRecursive(tt.args.s1, tt.args.s2); got != tt.want {
+					t.Errorf("LevRecursive() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestLev(t *testing.T) {
+	type args struct {
+		source string
+		dest   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"/apple=5", args{"", "apple"}, 5},
+		{"apple/=5", args{"apple", ""}, 5},
+		{"apple/apple=0", args{"apple", "apple"}, 0},
+		{"apple/dapple=1", args{"apple", "dapple"}, 1},
+		{
+			"alfabet/alfabet-1=1",
+			args{"abcdefghijklmnopqrstuvxyzåäö", "abcdefghijklmnopqrstuvxyzåä"}, 1,
+		},
+		{"long/long=24", args{"sdfpioudfglkjaweoiulknsdasdasd", "cxvpouwejk,mnasdljkhasdlkhasdiu"}, 23},
+		{"cattle/apple=1", args{"cattle", "apple"}, 3},
+		{"foo/bar=3", args{"foo", "bar"}, 3},
+		{"foo/foooooo=4", args{"foo", "foooooo"}, 4},
+		{"foooooo/foo=4", args{"foooooo", "foo"}, 4},
+		{"世界世界世界/foo=6", args{"世界世界世界", "foo"}, 6},
+		{"Test #1", args{"apple", "dapple"}, 1},
+		{"Test #2", args{"dapple", "apple"}, 1},
+		{"Identical", args{"apple", "apple"}, 0},
+		{"Add", args{"apple", "apples"}, 1},
+		{"Remove", args{"apples", "apple"}, 1},
+		{"Substitute", args{"apples", "applet"}, 1},
+		{"Test #1", args{"apple", "dapple"}, 1},
+		{"Test #2", args{"dapple", "apple"}, 1},
+		{"Test #3", args{"apple", "opple"}, 1},
+		{"Test #4", args{"kitten", "sitting"}, 3},
+		{"Test #5", args{"sunday", "saturday"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := Lev(tt.args.source, tt.args.dest); got != tt.want {
 					t.Errorf("Lev() = %v, want %v", got, tt.want)
 				}
 			},
