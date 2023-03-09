1e6c830ab4c8f323b4542536a0efe03fdedcef3c
per <per@softteam.se>
2023-03-09 13:38:29 +0100

    Recursive removed

--- levenshtein.go
+++ levenshtein.go
@@ -1,11 +1,5 @@
 package levenshtein
 
-import (
-	"fmt"
-)
-
-var memo map[string]int
-
 func Lev(source, dest string) int {
 	return lev([]rune(source), []rune(dest))
 }
@@ -43,56 +37,7 @@ func lev(source, dest []rune) int {
 		}
 	}
 
-	fmt.Println(d)
 	return d[m][n]
-}
-
-// LevRecursive : Returns the Levenshtein distance between two strings.
-func LevRecursive(source, dest string) int {
-	// Create the memoization map
-	memo = make(map[string]int)
-	return levRecursive([]rune(source), []rune(dest))
-}
-
-func levRecursive(source, dest []rune) int {
-	s := string(source)
-	d := string(dest)
-
-	// Check memoization map
-	m, ok := memo[s+d]
-	if ok {
-		return m
-	}
-
-	// Check empty source or dest
-	if len(source) == 0 {
-		memo[d] = len(dest)
-		return memo[d]
-	}
-	if len(dest) == 0 {
-		memo[s] = len(source)
-		return memo[s]
-	}
-
-	s1 := string(source[1:])
-	d1 := string(dest[1:])
-
-	// Check if first char/rune is equal
-	if source[0] == dest[0] {
-		l := levRecursive(source[1:], dest[1:])
-		memo[s1+d1] = l
-		return l
-	}
-
-	l1 := levRecursive(source[1:], dest)     // Remove first letter in source
-	l2 := levRecursive(source, dest[1:])     // Add one letter to source
-	l3 := levRecursive(source[1:], dest[1:]) // Replace first letter in source
-
-	memo[s1+d] = l1
-	memo[s+d1] = l2
-	memo[s1+d1] = l3
-
-	return 1 + min(l1, l2, l3)
 }
 
 func min(lev ...int) int {
